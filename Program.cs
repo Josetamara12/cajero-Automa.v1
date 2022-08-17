9473f4cb9cd1211695a1e772aead02695a6338f1
Josetamara12 <jose.tamara426@pascualbravo.edu.co>
2022-08-16 19:15:59 -0500

    Se iniciliza app cajeroAut

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cajeroAutomatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Usuario1, Usuario2, Usuario3;
+            Console.ForegroundColor = ConsoleColor.Gray; 
+            Console.WriteLine("Por favor ingresa tu nombre: ");
+            Usuario1 = Console.ReadLine();
+            Console.WriteLine("Por favor ingresa tu nombre: ");
+            Usuario2 = Console.ReadLine();
+            Console.WriteLine("Por favor ingresa tu nombre: ");
+            Usuario3 = Console.ReadLine();
+
+            Console.ReadKey(); 
+
+
+        }
+    }
+}
